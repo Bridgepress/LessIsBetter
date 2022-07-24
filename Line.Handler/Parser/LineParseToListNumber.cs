3dc269bff2a4471b1c373b3f425ea92b7926b19a
BridgePress <lbammargeral@gmail.com>
2022-07-24 17:09:40 +0300

    Add project files.

---
+++ Line.Handler/Parser/LineParseToListNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line.Handler
+{
+    public class LineParseToListNumber : IParser
+    {
+        private string _line;
+        public List<int> ListNumbers;
+
+        public LineParseToListNumber(string line)
+        {
+            ListNumbers = new List<int>();
+            _line = line;
+        }
+
+        public void Parse()
+        {
+            string newLine = _line.Trim();
+            string[] array = newLine.Split(' ');
+
+            foreach (string item in array)
+            {
+                int number = int.Parse(item);
+                ListNumbers.Add(number);
+            }
+        }
+    }
+}
---
+++ Line.Handler/SortBySumOfNumbersInNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line.Handler
+{
+    public class SortBySumOfNumbersInNumber
+    {
+        private List<int> _listNumbers;
+
+        public SortBySumOfNumbersInNumber(List<int> listNumbers)
+        {
+            _listNumbers = listNumbers;
+        }
+
+        public void Sort()
+        {
+            List<int> sortNumbers = _listNumbers.Sort((x, y) => )
+        }
+    }
+}
