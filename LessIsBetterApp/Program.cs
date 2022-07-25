4b1016da0c66f0e769b54ed9143ed677206f161f
BridgePress <lbammargeral@gmail.com>
2022-07-25 13:57:50 +0300

    Edit Main

--- LessIsBetterApp/Program.cs
+++ LessIsBetterApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Line.Handler;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,13 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LessIsBetter
+namespace LessIsBetterApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            LessIsBetter lessIsBetter = new LessIsBetter(Console.ReadLine());
+            LessIsBetter lessIsBetter = new LessIsBetter();
+            Console.WriteLine(lessIsBetter.Order(Console.ReadLine()));
             Console.ReadKey();
         }
     }
