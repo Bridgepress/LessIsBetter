7c03c1072aab9b68906caa1d71928547d0afc832
BridgePress <lbammargeral@gmail.com>
2022-07-25 13:54:45 +0300

    Add class LessIsBetter

---
+++ Line.Handler/LessIsBetter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line.Handler
+{
+    public class LessIsBetter
+    {
+        public string Order(string input)
+        {
+            LineParseToListNumber lineParseToListNumber = new LineParseToListNumber(input);
+            lineParseToListNumber.Parse();
+            SortBySumOfNumbersInNumber sortBySumOfNumbersInNumber = new SortBySumOfNumbersInNumber(lineParseToListNumber.ListNumbers);
+            sortBySumOfNumbersInNumber.Sort();
+
+            return string.Join(" ", sortBySumOfNumbersInNumber.SortedNumbers);
+        }
+    }
+}
